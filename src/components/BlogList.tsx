98cae4e6d8695f3154c34fcda44497c81f4bfde6
mjkushman <mjkushman@gmail.com>
2025-01-13 21:48:06 -0800

    Blog loader and footer styling

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -4,33 +4,43 @@ import Link from "next/link";
 type Props = {};
 
 const BlogList = async (props: Props) => {
-  let posts;
+  let posts = [];
   try {
     const { data, status } = await autoBloggerFetch(`posts`);
+    // console.log(data[0])
     posts = data;
   } catch (error) {
     console.log("api request failed:", error);
   }
 
+  if(posts.length == 0) return "No posts" 
   return (
     <div className="my-10">
-      {posts.map((p) => (
-        <div key={p.postId}>
-          <div className="my-2">
-            <div>
-            <small className="align-bottom">
-            {new Date(p.createdAt).toLocaleDateString("en-US", {
-              month: "short",
-              day: "numeric",
-              year: "numeric",
-            })}</small>
+      {posts.length === 0 ? (
+        <div>No posts found.</div>
+      ) : (
+        posts.map((p) => (
+          <div key={p.postId}>
+            <div className="my-2">
+              <Link
+                href={`blog/${p.postId}`}
+                className="hover:underline decoration-[--primary] underline-offset-2"
+              >
+                {p.title}
+              </Link>
+              <div className="mb-4">
+                <small className="align-bottom">
+                  {new Date(p.createdAt).toLocaleDateString("en-US", {
+                    month: "short",
+                    day: "numeric",
+                    year: "numeric",
+                  })}
+                </small>
+              </div>
             </div>
-            <Link href={`blog/${p.postId}`} className="prose hover:underline decoration-[--primary] underline-offset-2">
-              {p.title}
-            </Link>
           </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 };
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,28 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 
 const Footer = () => {
-  const element = <FontAwesomeIcon icon={faEnvelope} />;
-  return (
-    <footer className="w-full h-20 bg-red-400">
-        <div className="flex flex-row justify-center items-center">
+  const mail = <FontAwesomeIcon icon={faEnvelope} size="xs" />;
+  const github = (
+    <FontAwesomeIcon icon={faGithub} size="xs" href="https://google.com" />
+  );
+const linkedIn = <FontAwesomeIcon icon={faLinkedin} size="xs" />
 
-      <div className="w-12">
-      Element?: {element}
+  return (
+    <footer className="w-full h-20 justify-items-center flex items-center justify-center">
+      <div className="flex flex-row gap-4 items-center">
+        <a href="mailto:mikesextras11+portfolio@gmail.com" target="_blank">
+          <div className="w-6">{mail}</div>
+        </a>
+        <a href="https://github.com/mjkushman" target="_blank">
+          <div className="w-6">{github}</div>
+        </a>
+        <a href="https://www.linkedin.com/in/mjkushman/" target="_blank">
+          <div className="w-6">{linkedIn}</div>
+        </a>
       </div>
-        </div>
     </footer>
   );
 };
