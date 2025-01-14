440bd835e92375e482c62574948832e9afa16003
mjkushman <mjkushman@gmail.com>
2025-01-13 21:47:29 -0800

    blog loading

--- src/app/blog/Loading.tsx
+++ src/app/blog/Loading.tsx
@@ -1,10 +1,12 @@
+"use client"
 import React from 'react'
+import SyncLoader from 'react-spinners/SyncLoader'
 
 type Props = {}
 
 const Loading = (props: Props) => {
   return (
-    <div>Loading....</div>
+    <div><SyncLoader/></div>
   )
 }
 
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,26 +1,34 @@
+import BlogList from "@/components/BlogList";
+import Link from "next/link";
+import React from "react";
 
-
-
-import BlogList from '@/components/BlogList'
-import Link from 'next/link'
-import React from 'react'
-
-type Props = {}
+type Props = {};
 
 const Blog = (props: Props) => {
   return (
     <div className="block max-w-xl justify-center">
-    <h1 className="text-5xl font-semibold tracking-wide mb-10">Blog</h1>
-    <p>This blog is entirely written by AI. <br/> 
-    It&apos;s powered by my project <Link href={"https://autoblogger-client.onrender.com/"} className='hover:underline decoration-[--primary] underline-offset-2'>Autoblogger</Link>. Using Autoblogger I set up an agent to periodically write about topics that interest me. Then I render those posts here.</p>
+      <h1 className="text-5xl font-semibold tracking-wide mb-10">Blog</h1>
+      <p>
+        This blog is entirely written by AI. <br />
+        It&apos;s powered by my project{" "}
+        <Link
+          href={"https://autoblogger-client.onrender.com/"}
+          className="hover:underline decoration-[--primary] underline-offset-2"
+        >
+          Autoblogger
+        </Link>
+        . With Autoblogger, I set up an agent to periodically write about topics
+        that interest me. The posts are rendered here.
+      </p>
+      <p>
+        Content in this blog is purely demonstrative.
+      </p>
 
-    <div>
-    <BlogList/>
-
+      <div>
+        <BlogList />
+      </div>
     </div>
-    
-  </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
