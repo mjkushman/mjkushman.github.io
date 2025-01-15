92dd5971cdfd3561e9e9f0a21d1b07857aec1df4
mjkushman <mjkushman@gmail.com>
2025-01-15 09:00:44 -0800

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",  // <=== enables static exports
 
-  basePath: "/portfolio",
+  basePath: "/",
   images: {
     unoptimized: true,
     localPatterns: [
